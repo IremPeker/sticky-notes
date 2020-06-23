d73b395c9c3456539ec88e89677336ad9fe76e68
unknown <irem.peker@gmx.de>
2020-06-23 10:18:45 +0200

    Added new elements, redifened the class names, edited the styling, fix the functionality

--- js/main.js
+++ js/main.js
@@ -1,116 +1,99 @@
 $(document).ready(function() {
-  console.log(`document is ready...`);
-
-  // this is the textarea in html
-  let textareaVal = document.getElementById("text");
-
-  let toDo =
+  const textArea = document.getElementById("text-area");
+  const toDo =
     localStorage.getItem("todo") == null
       ? []
       : JSON.parse(localStorage.getItem("todo"));
 
   // create new div
-  createNew = item => {
-    let spanClose = "✖";
-    let spanSave = "✔";
-    $("#notes").append(
-      `<div class="createNewDiv"><textarea class="inputVal">${item.item}</textarea><span id="${item.id}" class="close">${spanClose}</span><span class="save">${spanSave}</span></div>`
+  createNew = newToDo => {
+    const closeButton = "✖";
+    const saveButton = "✔";
+    $("#notes-wrapper").append(
+      `<div class="newItem"><textarea class="new-text-area">${newToDo.item}</textarea><button id="${newToDo.id}" class="close-button">${closeButton}</button><button class="save-button">${saveButton}</button><p class="date-time">Created At: ${newToDo.dateAndTime}</p></div>`
     );
   };
 
   // save on local storage
-  $(".saveInput").on("click", function(e) {
-    console.log(`create button onclick...`);
+  $(".input-save").on("click", function(e) {
     e.preventDefault();
     getToDo();
-    textareaVal.value = "";
+    textArea.value = "";
   });
 
-  getToDo = item => {
-    console.log(`inside getToDo function`);
-
-    console.log(`textareaVal.value=>`, textareaVal.value);
+  getToDo = newToDo => {
     textIdGen();
+    getDateAndTime();
 
-    // this is the object that I will push into the ToDo array
-    item = {
-      item: textareaVal.value,
-      id: textId
+    // the object 
+    newToDo = {
+      item: textArea.value,
+      id: textId,
+      dateAndTime: createdAt 
     };
 
     if (toDo == null) toDo = [];
-    toDo.push(item);
-    console.log(`todo array after push=>`, toDo);
-
-    console.log(`I will set item...`);
-
+    toDo.push(newToDo);
     localStorage.setItem("todo", JSON.stringify(toDo));
-
-    createNew(item);
+    createNew(newToDo);
   };
 
   toDo.forEach(item => {
-    console.log(`item inside for each=>`, item);
     createNew(item);
   });
 
-  // delete specific item
+  // delete 
 
-  $(".createNewDiv").on("click", "span.close", function(e) {
-    console.log(`delete onclick...`);
-
+  $(".close-button").on("click", function(e) {
     // find the id of the target
-    let targetClose = e.target.id;
-
+    const targetClose = e.target.id;
     for (let i = 0; i < toDo.length; i++) {
       if (targetClose == toDo[i].id) e.target.parentElement.remove();
     }
     // remove from toDo array
-    let index = toDo.findIndex(x => x.id == targetClose);
+    const index = toDo.findIndex(x => x.id == targetClose);
     toDo.splice(index, 1);
-
     // set localStorage again with the latest version of toDo array
     localStorage.setItem("todo", JSON.stringify(toDo));
   });
 
-  // update specific item
+  // update 
 
-  $(".createNewDiv").on("click", "span.save", function(e) {
-    console.log(`update onclick...`);
-    console.log(`toDo array before update=>`, toDo);
-
-    let targetUpdate = e.target.previousSibling.id;
-    let newValue = document.getElementsByClassName("inputVal");
-
+  $(".save-button").on("click", function(e) {
+    const targetUpdate = e.target.previousSibling.id;
+    const newValue = document.getElementsByClassName("new-text-area");
+    const newDate = document.lastModified;
+    const dateTime = document.getElementsByClassName("date-time");
     // find the index of the selected item
-    let index = toDo.findIndex(x => x.id == targetUpdate);
-    console.log(`index of the updated item is=>`, index);
-    console.log(`newValue of the specific item is`, newValue[index].value);
-
-    console.log(`before update item is=>`, toDo[index].item);
-
+    const index = toDo.findIndex(x => x.id == targetUpdate);
     // update the element inside the toDo array
     toDo[index].item = newValue[index].value;
-
-    console.log(`after update=>`, toDo[index].item);
-
+    toDo[index].dateAndTime = newDate;
+    dateTime[index].textContent = `Updated At: ${newDate}`;
     // set localStorage again with the latest version of toDo array
     localStorage.setItem("todo", JSON.stringify(toDo));
-    console.log(`toDo array after update=>`, toDo);
   });
 
   // how-to use explanation onclick
 
   $(".flip-input").on("click", ".how-to", function() {
-    console.log(`how to onclick....`);
-
     $(".flip-input").toggleClass("flipped");
   });
 
-  // this is to generate unique id's for close spans
+  // generate unique id's for close buttons
   textIdGen = () => {
     for (let i = 0; i <= localStorage.length; i++) {
       textId = new Date().getTime();
     }
   };
+
+  // get date for each toDo
+  getDateAndTime = () => {
+    const today = new Date();
+    const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    createdAt = date+' '+time;
+  }
 });
+
+
